682ab98b08146d6331a9169118d6e465c2c46468
brockmortenson <mortensonbrock@gmail.com>
2024-11-22 02:11:22 -0700

    commit

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5531ada84edf13c92f9569d9ab4bfe08483e05cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f04a9b6881afb5a7018080e7f77bf01df0160e46")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
